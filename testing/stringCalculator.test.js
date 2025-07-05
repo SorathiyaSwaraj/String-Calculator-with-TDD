393f2b692c80bcc3a7f1faa75b20ad34962e8f2a
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2025-07-05 14:22:36 +0530

    Task 1 test case - 2 added
    for single number should return number

--- testing/stringCalculator.test.js
+++ testing/stringCalculator.test.js
@@ -6,4 +6,8 @@ describe("Simple test cases - Task 1", () => {
         expect(add("")).toBe(0);
     });
 
+    test("for single number returns number", () => {
+        expect(add("8")).toBe(8);
+    });
+
 });
