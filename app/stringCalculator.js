575a8d6f1e29ff887166f2b326c513829b3242b1
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2025-07-05 15:47:04 +0530

    test case - 7 passed
    throws an error on negative number

--- app/stringCalculator.js
+++ app/stringCalculator.js
@@ -10,7 +10,13 @@ function add(numbers)
         numbers = numbers.slice(4);
     }
 
-    let parts = numbers.split(delimiter).map(Number);
+    let parts = numbers.split(delimiter).map(num => {
+        num = Number(num);
+        if(num < 0)
+            throw new Error("“negative numbers not allowed");
+
+        return num;
+    });
     return parts.reduce((acc, num) => acc + num, 0);
 }
 
