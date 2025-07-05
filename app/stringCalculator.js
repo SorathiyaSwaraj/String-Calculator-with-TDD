cdca8405ea04c900149696d2f0cc32ce42511835
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2025-07-05 15:08:49 +0530

    Task 3 completed with test case - 5 passed
    handles the new line between the numbers

--- app/stringCalculator.js
+++ app/stringCalculator.js
@@ -3,7 +3,7 @@ function add(numbers)
     if(numbers === "")
         return 0;
 
-    let parts = numbers.split(',').map(Number);
+    let parts = numbers.split(/,|\n/).map(Number);
     return parts.reduce((acc, num) => acc + num, 0);
 }
 
