e067355a4c001e4aedc6c033e641f83e5a4fea13
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2025-07-05 14:34:27 +0530

    test case - 3 passed
    returns sum for two numbers in input

--- app/stringCalculator.js
+++ app/stringCalculator.js
@@ -3,7 +3,11 @@ function add(numbers)
     if(numbers === "")
         return 0;
 
-    return Number(numbers);
+    let parts = numbers.split(',');
+    if(parts.length == 1)
+        return Number(parts[0]);
+    else
+        return Number(parts[0]) + Number(parts[1]);
 }
 
 module.exports = { add }
