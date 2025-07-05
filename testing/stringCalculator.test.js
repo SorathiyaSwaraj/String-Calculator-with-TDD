e845c3e1e077777c4d9b60bb1b673d8f7c64b08f
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2025-07-05 14:14:58 +0530

    Task 1 test case 1 added
    for empty string should return 0

--- testing/stringCalculator.test.js
+++ testing/stringCalculator.test.js
@@ -1,2 +1,9 @@
 const { add } = require("../app/stringCalculator")
 
+describe("Simple test cases - Task 1", () => {
+
+    test("for empty string returns 0", () => {
+        expect(add("")).toBe(0);
+    });
+
+});
